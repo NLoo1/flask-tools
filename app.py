a8dd6de402acb07359e6a60e0efff2b9ea1e702f
Nicholas L <NLoo1@outlook.com>
2024-01-08 18:44:08 -0500

    Refactored and added session storage

--- app.py
+++ app.py
@@ -10,59 +10,47 @@ app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
 debug = DebugToolbarExtension(app)
 
-# responses = []
-question_num = 0
-
 def helper(num):
     title = surveys.get("satisfaction").title
     question = surveys.get("satisfaction").questions[int(num)]
     return render_template("questions.html", num=num, title=title, question=question.question, choices= question.choices)
 
+@app.route('/questions', methods=["POST"])
+def start_questions():
+    return redirect('/questions/' + str(session['question_num']))
+
 @app.route("/")
 def home():
+    session['question_num'] = 0
+    session['responses'] = []
     survey = surveys.get("satisfaction")
     return render_template("home.html", title=survey.title, instructions=survey.instructions)
 
-# Display first question after posting from root
-@app.route("/questions/0", methods=["POST"])
-def get_question():
-        session["responses"] = []
-        if question_num >= len(surveys.get("satisfaction").questions):
-            flash("Thank you for your response.")
-            return redirect("/thankyou")
-        else:
-            return helper(question_num)
-
-# From a submitted response, go to answer and append response. Then, redirect to next question        
-@app.route("/answer")
-def add_response():
-    # global responses
-    data=list(request.args.items())
-    session["responses"].append(data[0][0])
-    global question_num
-    question_num = question_num+1
-    return redirect("/questions/" + str(question_num))
-
 # Displays next question. Should be accessed after redirection to /answer
-@app.route("/questions/<num>")
+@app.route("/questions/<num>", methods=["GET", "POST"])
 def next_question(num):
-    global question_num
-    if question_num < int(num):
+    if session['question_num'] < int(num):
         flash("Please complete questions in order. ")
-        return redirect("/questions/" + str(question_num))
+        return redirect("/questions/" + str(session['question_num']))
     # If question_num exceeds number of questions, redirect to thank you
-    elif question_num > len(surveys.get("satisfaction").questions): 
+    elif session['question_num'] > len(surveys.get("satisfaction").questions): 
         return redirect("/thankyou")
     else:
         try:
            return helper(num)   
         except:
-            # global responses
-            question_num = 1000000
+            session['question_num'] = 1000000
             return redirect("/thankyou")        
+        
+# From a submitted response, go to answer and append response. Then, redirect to next question        
+@app.route("/answer")
+def add_response():
+    data=list(request.args.items())
+    session["responses"].append(data[0][0])
+    session['question_num'] +=1
+    return redirect("/questions/" + str(session['question_num']))
+
 @app.route("/thankyou")
 def show_thanks():
-    raise
-    # print(session["responses"])
     return render_template("thankyou.html")
 
